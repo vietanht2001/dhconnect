c68d4a23f3c3d6937d99afbe0f53c70fb1cb2232
vietanht2001 <70068426+vietanht2001@users.noreply.github.com>
2023-04-04 23:01:46 +0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import Layout from '@/components/layout/Layout';
+import ArrowLink from '@/components/links/ArrowLink';
+import ButtonLink from '@/components/links/ButtonLink';
+import UnderlineLink from '@/components/links/UnderlineLink';
+import UnstyledLink from '@/components/links/UnstyledLink';
+import Seo from '@/components/Seo';
+
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+import Vercel from '~/svg/Vercel.svg';
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+// export default function HomePage() {
+//   return (
+//     <Layout>
+//       {/* <Seo templateTitle='Home' /> */}
+//       <Seo />
+
+//       <main>
+        
+//         <section className='bg-white'>
+//           <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
+//             <Vercel className='text-5xl' />
+//             <h1 className='mt-4'>
+//               Next.js + Tailwind CSS + TypeScript Starter
+//             </h1>
+//             <p className='mt-2 text-sm text-gray-800'>
+//               A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
+//               Import, Seo, Link component, pre-configured with Husky{' '}
+//             </p>
+//             <p className='mt-2 text-sm text-gray-700'>
+//               <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
+//                 See the repository
+//               </ArrowLink>
+//             </p>
+
+//             <ButtonLink className='mt-6' href='/components' variant='light'>
+//               See all components
+//             </ButtonLink>
+
+//             <UnstyledLink
+//               href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
+//               className='mt-4'
+//             >
+//               {/* eslint-disable-next-line @next/next/no-img-element */}
+//               <img
+//                 width='92'
+//                 height='32'
+//                 src='https://vercel.com/button'
+//                 alt='Deploy with Vercel'
+//               />
+//             </UnstyledLink>
+
+//             <footer className='footer p-10 bg-base-200 text-base-content'>
+//               <div>
+//                 <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule='evenodd' clipRule='evenodd' className='fill-current'></svg>
+//               </div>
+//               <div>
+//                 <span className="footer-title">Services</span> 
+//                 <a className="link link-hover">Branding</a>
+//                 <a className="link link-hover">Design</a>
+//                 <a className="link link-hover">Marketing</a>
+//                 <a className="link link-hover">Advertisement</a>
+//               </div> 
+//             </footer>
+//           </div>
+//         </section>
+//       </main>
+//     </Layout>
+//   );
+// }
+
+//Menu
+const menu=document.querySelector("#menu")
+const menuButton=document.querySelector("#menu-button")
+
+menuButton.addEventListener('click',()=>{
+  menu.classList.toggle('invisible');
+})
